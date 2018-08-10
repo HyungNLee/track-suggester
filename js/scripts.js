2b0791becc22ddc014d2d54ca3e2999c1ae797b6
Hyung Lee <hyungnaelee@gmail.com>
2018-08-10 11:33:59 -0700

    Add all questions in mainQuestion section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+  $("#userName").submit(function(event) {
+    var nameInput = $("input#name").val();
+    $(".nameReturn").text(nameInput); //Need to add .nameReturn spans to results.
+  })
 
 
 
 
-  
 })
